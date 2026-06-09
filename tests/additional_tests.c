cdc3901e0307962c035f49282d8ea7f5fd9a22fd
Alexander Lindgren <a.lindgren251@proton.me>
2026-06-09 20:47:49 +0200

    style: apply clang-format after merging codeberg changes

--- tests/additional_tests.c
+++ tests/additional_tests.c
@@ -5,7 +5,7 @@ CBELT_GROUP("Additional Tests")
 CBELT_TEST(additional_test) { return TEST_SUCCESS; }
 
 CBELT_TEST_TIMEOUT(infinite_loop, 2) {
-  for (;;)
-    ;
-  return TEST_SUCCESS;
+   for (;;)
+      ;
+   return TEST_SUCCESS;
 }
--- tests/test_tests.c
+++ tests/test_tests.c
@@ -15,8 +15,8 @@ static int test_teardown_called = 0;
  * Global setup / teardown
  *--------------------------------------------------------------------------*/
 CBELT_GLOBAL_SETUP() {
-  global_setup_called = 1;
-  cbelt_set_default_timeout(10);
+   global_setup_called = 1;
+   cbelt_set_default_timeout(10);
 }
 
 CBELT_GLOBAL_TEARDOWN() { global_teardown_called = 1; }
